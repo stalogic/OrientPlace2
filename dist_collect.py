47a6c7063bb85ce282e311ba98c82c63bf3eb6e9
stalogic <jiangmings1992@163.com>
2025-04-07 19:42:54 +0800

    update

--- dist_collect.py
+++ dist_collect.py
@@ -2,7 +2,7 @@ import gym
 import os
 import place_env
 from placedb import LefDefReader, build_soft_macro_placedb
-
+from model import OrientPPO
 import reverb
 
 PROJECT_ROOT = "/home/jiangmingming/mntspace/OrientPlace2"
@@ -18,9 +18,32 @@ reader = LefDefReader(data_root, design_name, cache_root)
 placedb = build_soft_macro_placedb(reader)
 
 env = gym.make("orient_env-v0", placedb=placedb, grid=224).unwrapped
+agent = OrientPPO(len(placedb.macro_info), grid=224, num_game_per_update=5, batch_size=128, lr=1e-5, gamma=0.98, device='cpu')
+agent.CANVAS_SLICE = env.CANVAS_SLICE
+agent.WIRE_SLICE = env.WIRE_SLICE
+agent.POS_SLICE = env.POS_SLICE
+agent.FEATURE_SLICE = env.FEATURE_SLICE
+
+
+model_info = reverb.TimestepDataset.from_table_signature(
+    server_address="localhost:12888",
+    table="model_info",
+    max_in_flight_samples_per_worker=1
+)
+
 
 with client.trajectory_writer(num_keep_alive_refs=200) as writer:
     while True:
+        for model in model_info.take(1):
+            for key, value in model.data.items():
+                # print(f"{key=}, {type(value)}")
+                if key == "model_id":
+                    print(f"{key=}, {value.numpy().item()=}")
+                else:
+                    print(f"{key=}, {len(value)=}")
+
+        continue
+        
         state = env.reset()
         done = False
         while not done:
--- dist_reverb.py
+++ dist_reverb.py
@@ -1,8 +1,64 @@
 import reverb
+import os
+import gym
 import tensorflow as tf
+import time
+import torch
+import numpy as np
+import place_env
+from placedb import LefDefReader, build_soft_macro_placedb
 PROJECT_ROOT = "/home/jiangmingming/mntspace/OrientPlace2"
 
-signature = {
+from model import OrientPPO
+
+data_root = os.path.join(PROJECT_ROOT, "benchmark")
+design_name = "ariane133"
+cache_root = os.path.join(PROJECT_ROOT, "cache")
+reader = LefDefReader(data_root, design_name, cache_root)
+placedb = build_soft_macro_placedb(reader)
+
+env = gym.make("orient_env-v0", placedb=placedb, grid=224).unwrapped
+
+
+placed_num_macro = len(placedb.macro_info)
+agent = OrientPPO(placed_num_macro, grid=224, num_game_per_update=5, batch_size=128, lr=1e-5, gamma=0.98, device='cpu')
+agent.CANVAS_SLICE = env.CANVAS_SLICE
+agent.WIRE_SLICE = env.WIRE_SLICE
+agent.POS_SLICE = env.POS_SLICE
+agent.FEATURE_SLICE = env.FEATURE_SLICE
+
+variables = {
+    'orient_actor': agent.orient_actor_net.state_dict(),
+    'place_actor': agent.place_actor_net.state_dict(),
+    'model_id': torch.from_numpy(np.array(0, dtype=np.int32))
+}
+
+def to_tf_dtype(torch_dtype):
+    dtype_mapping = {
+        torch.float32: tf.float32,
+        torch.float64: tf.float64,
+        torch.int32: tf.int32,
+        torch.int64: tf.int64,
+        torch.bool: tf.bool
+    }
+    return dtype_mapping[torch_dtype]
+
+model_signature = tf.nest.map_structure(
+    lambda var: tf.TensorSpec(var.shape, to_tf_dtype(var.dtype)),
+    variables
+)
+
+model_table = reverb.Table(
+    name="model_info",
+    sampler=reverb.selectors.Fifo(),
+    remover=reverb.selectors.Fifo(),
+    rate_limiter=reverb.rate_limiters.MinSize(1),
+    max_size=1,
+    max_times_sampled=0,
+    signature=model_signature
+)
+
+data_signature = {
     'state': tf.TensorSpec(shape=[None, 852995], dtype=tf.float64),
     'orient': tf.TensorSpec(shape=[None], dtype=tf.int64),
     'action': tf.TensorSpec(shape=[None], dtype=tf.int64),
@@ -13,28 +69,45 @@ signature = {
     'done': tf.TensorSpec(shape=[None], dtype=tf.bool)
 }
 
-table = reverb.Table(
+data_table = reverb.Table(
     name="experience",
     sampler=reverb.selectors.MaxHeap(),
     remover=reverb.selectors.MinHeap(),
     max_size=10000,
-    rate_limiter=reverb.rate_limiters.MinSize(1),
-    signature=signature
+    rate_limiter=reverb.rate_limiters.MinSize(5),
+    signature=data_signature
 )
 
-server = reverb.Server([table], port=12888)
-
+server = reverb.Server([model_table, data_table], port=12888)
 dataset = reverb.TrajectoryDataset.from_table_signature(
     server_address="localhost:12888",
     table='experience',
     max_in_flight_samples_per_worker=2
 )
 
-for sample in dataset.take(2):
-    print(sample.info)
-    for key, value in sample.data.items():
-        print(f"{key=} shape: {value.shape} dtype: {value.dtype}, numpy.shape: {value.numpy().shape}")
+client = reverb.Client("localhost:12888")
 
 
-# 关闭服务器
+
+for _ in range(10):
+    variables = {
+        'orient_actor': agent.orient_actor_net.state_dict(),
+        'place_actor': agent.place_actor_net.state_dict(),
+        'model_id': variables['model_id'] + 1
+    }
+    client.insert([variables], priorities={'model_info': variables['model_id'].numpy().item()})
+
+    for sample in dataset.take(2):
+        # print(sample.info)
+        for key, value in sample.data.items():
+            print(f"{key=} shape: {value.shape} dtype: {value.dtype}, numpy.shape: {value.numpy().shape}")
+    time.sleep(100)
+
+
+
+
+
+
+
+# # 关闭服务器
 server.stop()
